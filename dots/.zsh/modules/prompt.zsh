3ade44dbc19a0e1b43bd317d7880a29701930881
fallwith <fallwith@gmail.com>
2014-01-28 15:34:30 -0800

    zsh prompt: place a space character between the git branch and the koala emoji

--- dots/.zsh/modules/prompt.zsh
+++ dots/.zsh/modules/prompt.zsh
@@ -2,4 +2,4 @@ local ret_status="%(?:%{$green112%}$>:%{$red124%}$>%s)" # green/red indicates th
 eval emkoala=`echo -e '\xF0\x9F\x90\xA8'`
 GIT_BRANCH_PREFIX="%{$reset_color%}%{$fg[white]%}["
 GIT_BRANCH_SUFFIX="]%{$reset_color%}"
-PROMPT='%{$blue039%}%c%{$reset_color%} %{$grey242%}%T%{$reset_color%} $(git_branch)${emkoala}  ${ret_status}%{$green112%}%p%{$reset_color%} '
+PROMPT='%{$blue039%}%c%{$reset_color%} %{$grey242%}%T%{$reset_color%} $(git_branch) ${emkoala}  ${ret_status}%{$green112%}%p%{$reset_color%} '
