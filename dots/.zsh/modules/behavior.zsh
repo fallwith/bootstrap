fa031046f2af2132927d6c359893977c66c533c1
fallwith <fallwith@gmail.com>
2014-03-11 14:29:16 -0700

    zsh: move vi related content out into its own vi.zsh module - added ctrl-r and ctrl-c behavior to that new module

--- dots/.zsh/modules/behavior.zsh
+++ dots/.zsh/modules/behavior.zsh
@@ -4,13 +4,6 @@ setopt noclobber
 # disable 'rm *' prompting
 setopt rmstarsilent
 
-# enable emacs key bindings (ctrl-a, ctrl-e for start/end of line)
-# bindkey -e
-
-# enable vi key bindings (ESC+0, ESC+$ for start/end of line)
-bindkey -v
-export KEYTIMEOUT=1 # 0.1 sec delay between switching modes
-
 # allow comments (prefixed by '#') to be used in interactive shells
 setopt interactive_comments
 
---
+++ dots/.zsh/modules/vi.zsh
@@ -0,0 +1,21 @@
+# enable vi key bindings (ESC+0, ESC+$ for start/end of line)
+bindkey -v
+
+# 0.1 sec delay between switching modes
+export KEYTIMEOUT=1
+
+# function to indicate the current vi mode (i = insert, n = normal)
+VIMODE='i'
+function zle-line-init zle-keymap-select {
+ VIMODE="${${KEYMAP/vicmd/n}/(main|viins)/i}"
+ zle reset-prompt
+}
+zle -N zle-keymap-select
+zle -N zle-line-init
+
+# allow ctrl-r to search across history, as in emacs mode
+bindkey -M viins '^r' history-incremental-search-backward
+bindkey -M vicmd '^r' history-incremental-search-backward
+
+# allow ctrl-c to behave the same in normal mode as in insert mode
+bindkey -M vicmd '^c' self-insert
