e872ddb9fd75fb2778dfb716d7f7f36d70add916
fallwith <fallwith@gmail.com>
2014-10-29 08:02:17 -0700

    extra whitespace removal

--- dots/.zsh/modules/prompt.zsh
+++ dots/.zsh/modules/prompt.zsh
@@ -5,4 +5,3 @@ GIT_BRANCH_PREFIX=" %{$reset_color%}%{$fg[white]%}["
 GIT_BRANCH_SUFFIX="]%{$reset_color%}"
 
 PROMPT='%{$blue039%}%c%{$reset_color%} %{$grey242%}%T%{$reset_color%}$(git_branch) %{$voilet013%}${VIMODE} %{$reset_color%}${emkoala}  ${ret_status}%{$green112%}%p%{$reset_color%} '
-
