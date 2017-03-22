f2ea4701b5eca2c34041d5b0e33481cbe141c767
fallwith <fallwith@gmail.com>
2017-03-21 23:09:05 -0700

    zsh: don't use emoji in emacs shell prompts

--- dots/.zsh/modules/prompt.zsh
+++ dots/.zsh/modules/prompt.zsh
@@ -13,10 +13,11 @@ setopt prompt_subst # interpolate functions within the prompt string
 # eval emkoala=`echo -e "\U1F428"`
 emkoala=$'\U1F428'
 
-# emmoto=$'\U1F3CD'
-
 GIT_BRANCH_PREFIX=" %{$reset_color%}%{$fg[white]%}["
 GIT_BRANCH_SUFFIX="]%{$reset_color%}"
 
-PROMPT='%{$blue039%}%c%{$reset_color%} %{$grey242%}%T%{$reset_color%}$(git_branch) %{$violet013%}${VIMODE} %{$reset_color%}${emkoala}  ${ret_status}%{$green112%}%p%{$reset_color%} '
-# PROMPT='%{$blue039%}%c%{$reset_color%} %{$grey242%}%T%{$reset_color%}$(git_branch) %{$violet013%}${VIMODE} %{$reset_color%}${emkoala}  %{$bg[white]%}$emmoto% {$reset_color%} ${ret_status}%{$green112%}%p%{$reset_color%} '
+if [[ -n $INSIDE_EMACS ]]; then
+  PROMPT='%{$blue039%}%c%{$reset_color%} %{$grey242%}%T%{$reset_color%}$(git_branch) %{$violet013%}${VIMODE} %{$reset_color%}${ret_status}%{$green112%}%p%{$reset_color%} '
+else
+  PROMPT='%{$blue039%}%c%{$reset_color%} %{$grey242%}%T%{$reset_color%}$(git_branch) %{$violet013%}${VIMODE} %{$reset_color%}${emkoala}  ${ret_status}%{$green112%}%p%{$reset_color%} '
+fi
