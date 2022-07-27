4991bbca150d69e1d9bfcfad0ea1b74c5f990b65
fallwith <fallwith@gmail.com>
2022-07-27 01:45:23 -0700

    nvim: filetypes

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -19,7 +19,15 @@ local gitCommitGroup = vim.api.nvim_create_augroup('gitcommit', { clear = true }
 vim.api.nvim_create_autocmd(
   "Filetype",
   { pattern = "gitcommit",
-    command = "setlocal spell textwidth=72 colorcolumn=50,72" }
+    command = "setlocal spell textwidth=72 colorcolumn=50,72",
+    group = gitCommitGroup }
+)
+
+-- 'Envfile' files are Ruby
+local envfileGroup = vim.api.nvim_create_augroup('envfile', { clear = true })
+vim.api.nvim_create_autocmd(
+  { "Bufread" },
+  { pattern = "Envfile", command = "set filetype=ruby", group = envfileGroup }
 )
 
 -- null-ls
