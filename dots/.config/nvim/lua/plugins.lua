01ec5da67b9cc50f0e85e90792b2f565cedb9500
fallwith <fallwith@gmail.com>
2023-10-14 15:06:00 -0700

    nvim config updates

--- dots/.config/nvim/lua/plugins.lua
+++ dots/.config/nvim/lua/plugins.lua
@@ -16,7 +16,23 @@ require('lazy').setup({
   'tpope/vim-commentary',
 
   -- add, remove, swap surroundings like quotes or braces
-  'tpope/vim-surround',
+  --
+  --Old text                    Command         New text
+  --------------------------------------------------------------------------
+  --surr*ound_words             ysiw)           (surround_words)
+  --*make strings               ys$"            "make strings"
+  --[delete ar*ound me!]        ds]             delete around me!
+  --remove <b>HTML t*ags</b>    dst             remove HTML tags
+  --'change quot*es'            cs'"            "change quotes"
+  --<b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+  --delete(functi*on calls)     dsf             function calls
+  { "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end },
 
   -- add helpful closing structures (like 'end') for Ruby and others
   'tpope/vim-endwise',
