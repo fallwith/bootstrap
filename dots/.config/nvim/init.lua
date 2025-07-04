f93f6bae8639f28c41770ce04d6ff416e566ac5b
fallwith <fallwith@gmail.com>
2025-07-04 02:25:29 -0700

    nvim: sayonara

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -3,6 +3,7 @@ require "config.lazy"
 require "config.mappings"
 require "config.rails"
 require "config.autocmd"
+require("config.sayonara").setup()
 if vim.g.neovide then
   require "config.neovide"
 end
