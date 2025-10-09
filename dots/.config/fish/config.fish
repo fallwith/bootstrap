e75d8d72a090f989761a61b98321f4cd45886676
fallwith@gmail.com <fallwith@gmail.com>
2025-10-09 11:03:12 -0700

    swap out tsu, bring in 🦊

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -166,20 +166,10 @@ function fish_title
   end
 end
 
-if type -q tsu
-  set tsu_exists
-end
-
 function fish_prompt
   set -l last_status $status
 
-  # tsu driven tide info
-  if set -q tsu_exists
-    set -l tide_status (tsu)
-    set_color F8E6BD
-    echo -n "$tide_status "
-    set_color normal
-  end
+  echo -n ' 🦊 '
 
   # directory basename
   set_color 9DD6E7
