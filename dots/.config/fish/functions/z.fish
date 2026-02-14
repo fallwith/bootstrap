b99badd1294cc53a47aa24f8d01f1412040228bb
fallwith@gmail.com <fallwith@gmail.com>
2026-02-13 21:31:10 -0800

    z.fish: support worktree based clone dirs

--- dots/.config/fish/functions/z.fish
+++ dots/.config/fish/functions/z.fish
@@ -1,5 +1,13 @@
 function z --description 'Interactive directory selection using fzf'
-  set -l selected_dir (fd -td -tl -d1 . ~ ~/.config ~/git | sed "s|$HOME/||g" | fzf +m --height 33% --border --layout=reverse)
+  set -l search_roots ~ ~/.config ~/git
+
+  for dir in ~/git/*/
+    if test -d "$dir/.bare"
+      set -a search_roots $dir
+    end
+  end
+
+  set -l selected_dir (fd -td -tl -d1 . $search_roots | sed "s|$HOME/||g" | sort -u | fzf +m --height 33% --border --layout=reverse)
   if test -n "$selected_dir"
     cd $HOME/$selected_dir
   end
