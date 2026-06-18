612beca19427b0b112f64045f0f0c536db2ded00
fallwith@gmail.com <fallwith@gmail.com>
2026-06-17 21:24:37 -0700

    linear_mine.fish: show me my tickets

---
+++ dots/.config/fish/functions/linear_mine.fish
@@ -0,0 +1,10 @@
+function linear_mine -d 'List my open Linear tickets, sorted by workflow status'
+  linear-cli i list --mine --filter 'state.name!=Done' --filter 'state.name!=Canceled' -o json \
+    | jq -r '
+      def rank:
+        {"In Progress":0, "In Review":1, "Todo":2, "Backlog":3, "Triage":4}[.] // 99;
+      sort_by(.state.name | rank)[] |
+      "\(.identifier)\t[\(.state.name)]\t\(.title)"
+    ' \
+    | column -t -s \t
+end
