983b696f69ddf4d9e1367c29c9c3ca5879b473ee
fallwith@gmail.com <fallwith@gmail.com>
2026-07-15 17:01:30 -0700

    linear_mine.fish: fetch all records, not page 1

--- dots/.config/fish/functions/linear_mine.fish
+++ dots/.config/fish/functions/linear_mine.fish
@@ -1,5 +1,5 @@
 function linear_mine -d 'List my open Linear tickets, sorted by workflow status'
-  linear-cli i list --mine --filter 'state.name!=Done' --filter 'state.name!=Canceled' -o json \
+  linear-cli i list --mine --all --filter 'state.name!=Done' --filter 'state.name!=Canceled' -o json \
     | jq -r '
       def rank:
         {"In Progress":0, "In Review":1, "Todo":2, "Backlog":3, "Triage":4}[.] // 99;
